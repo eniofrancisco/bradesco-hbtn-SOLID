99ff574e551dedb926f969be7df5ea69005f95f4
eniofrancisco <eniofrancisco@gmail.com>
2025-10-30 18:13:45 +0000

    Clean Code

--- 0x04/cleanCode/Comments.java
+++ 0x04/cleanCode/Comments.java
@@ -10,10 +10,11 @@ public class Comments {
     }
 
     /**
-     * <p>Este método publica resultados com base na data.</p>
-     * @param date Data a partir da qual os resultados devem ser publicados
-     * @return Lista de entidades de resultado
-     * @since 16.0
+     * <p> este método publica resultados com base na data
+     * </p>
+     * @param data data a partir da qual os resultados devem ser publicados
+     * @retornar Lista de entidades de resultado
+     * @desde 16.0
      */
     public List<Result> publishResults(Date date) {
         // alguma lógica de negócios aqui
